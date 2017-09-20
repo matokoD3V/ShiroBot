24d2bed7e7a4ba6bee0ec9dd2feb14486032e24d
crbaldwin <charlesr.baldwinn@gmail.com>
2017-09-19 19:16:32 -0500

    Removed whitespace
    Removed whitespace in main.

--- src/ShiroBot.java
+++ src/ShiroBot.java
@@ -19,66 +19,39 @@ import net.dv8tion.jda.core.exceptions.RateLimitedException;
 public class ShiroBot {
     public static void main(String[] args) throws IOException, LoginException, IllegalArgumentException, RateLimitedException
     {
-        // the first is the bot token
-        String token = "MzU5MDg1MjE4ODM0OTM5OTA1.DKB3rg.pjE0eHMpv1T-5Qvt9FegoGFV7hI";
 
-        // the second is the bot's owner's id
+        String token = "MzU5MDg1MjE4ODM0OTM5OTA1.DKB3rg.pjE0eHMpv1T-5Qvt9FegoGFV7hI";
         String ownerId = "235995758896087041";
 
-        // define an eventwaiter, dont forget to add this to the JDABuilder!
         EventWaiter waiter = new EventWaiter();
-
-        // define a command client
         CommandClientBuilder client = new CommandClientBuilder();
 
-        // The default is "Type !!help" (or whatver prefix you set)
         client.useDefaultGame();
-
-        // sets the owner of the bot
         client.setOwnerId(ownerId);
-
-        // sets the bot prefix
         client.setPrefix("s_");
-
-        // adds commands
         client.addCommands(
                 // command to show information about the bot
                 new AboutCommand(Color.BLUE, "a retarded weeb bot.",
                         new String[]{"Niggers","Chicken","Watermelon"},
                         new Permission[]{Permission.ADMINISTRATOR}),
-
-                // command to show a random cat
-                //new CatCommand(),
-
-                // command to make a random choice
-                //new ChooseCommand(),
-
-                // command to say hello
-                //new HelloCommand(waiter),
-
-                new SetName(),
-                new SendBroadcast(),
-                new GetGirl(),
-                new JoinVoice(),
-                new LeaveVoice(),
-
-                new PingCommand(),
-
-                new ShutdownCommand());
+                        new SetName(),
+                        new SendBroadcast(),
+                        new GetGirl(),
+                        new JoinVoice(),
+                        new LeaveVoice(),
+                        new PingCommand(),
+                        new ShutdownCommand());
 
         // start getting a bot account set up
         new JDABuilder(AccountType.BOT)
                 // set the token
                 .setToken(token)
-
                 // set the game for when the bot is loading
                 .setStatus(OnlineStatus.DO_NOT_DISTURB)
                 .setGame(Game.of("loading..."))
-
                 // add the listeners
                 .addEventListener(waiter)
                 .addEventListener(client.build())
-
                 // start it up!
                 .buildAsync();
 
